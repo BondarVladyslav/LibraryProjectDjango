3423e7c2d977aa779dd882337951f33b0a7770a6
BondarVladyslav <1vladbondar1@gmail.com>
2026-06-10 17:29:51 +0300

    Added favourite func button

--- authorization/models.py
+++ authorization/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE , related_name='additional_data')
     favourite_books = models.ManyToManyField('books.Book', related_name='favourite_books', blank=True)
     bio = models.TextField(blank=True, null=True)
 
